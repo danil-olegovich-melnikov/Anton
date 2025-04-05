98940e1e3173fd8f5e13e649c43afb790c952500
Данил Мельников <danil.olegovich.melnikov@inbox.ru>
2025-04-05 08:33:14 +0600

    i

--- photo/urls.py
+++ photo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import DateListAPIView
+from .views import DateListAPIView, PaymentListView
 from .views import payment_webhook, create_payment_view
 
 urlpatterns = [
     path('dates/', DateListAPIView.as_view(), name='date-list'),
-    path("webhook/", payment_webhook, name="payment_webhook"),
+    path("payment_webhook/", payment_webhook, name="payment_webhook"),
     path("pay/", create_payment_view, name="create_payment"),
+    path('orders/', PaymentListView.as_view(), name='orders-list'),
 ]
