d92e613f9374bde82921087d79c07c63ba04440b
Данил Мельников <danil.olegovich.melnikov@inbox.ru>
2025-04-05 08:32:26 +0600

    i

--- photo/models.py
+++ photo/models.py
@@ -27,15 +27,30 @@ class Photo(models.Model):
 class Client(models.Model):
     email = models.EmailField("Почта", unique=True)
 
+    class Meta:
+        verbose_name = "Клиент"
+        verbose_name_plural = "Клиенты"
+
+    def __str__(self):
+        return self.email
 
 class Payment(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name="Клиент")
     url = models.CharField("Ссылка на оплату", max_length=256)
+    order_id = models.UUIDField(verbose_name='ID заказа')
+    is_paid = models.BooleanField("Оплачено", default=False)
 
+    def __str__(self):
+        return f"Заказ №{self.id}"
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
 
 class Order(models.Model):
     payment = models.ForeignKey(Payment, on_delete=models.CASCADE, verbose_name="Оплата")
     photo = models.ForeignKey(Photo, on_delete=models.CASCADE, verbose_name="Фотография")
-
+    
+    def __str__(self):
+        return f"Заказ {self.id}"
 
 
