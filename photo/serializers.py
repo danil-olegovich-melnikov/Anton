2716c38106152a2835e4b498e5b1f0f73f14e921
Данил Мельников <danil.olegovich.melnikov@inbox.ru>
2025-04-05 08:32:06 +0600

    i

--- photo/serializers.py
+++ photo/serializers.py
@@ -17,3 +17,36 @@ class DateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Date
         fields = ['id', 'date', 'photos']
+from rest_framework import serializers
+from .models import Date, Photo, Order, Payment
+
+class PhotoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Photo
+        fields = ['id', 'photo', 'price']
+
+class DateSerializer(serializers.ModelSerializer):
+    photos = PhotoSerializer(many=True, read_only=True, source='photo_set')
+    date = serializers.SerializerMethodField()
+
+    def get_date(self, obj):
+        return obj.date.strftime('%d.%m.%Y')
+    
+    class Meta:
+        model = Date
+        fields = ['id', 'date', 'photos']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    photo = PhotoSerializer()
+    
+    class Meta:
+        model = Order
+        fields = ['id', 'photo']
+
+class PaymentSerializer(serializers.ModelSerializer):
+    orders = OrderSerializer(many=True, read_only=True, source='order_set')
+    
+    class Meta:
+        model = Payment
+        fields = ['id', 'client', 'url', 'is_paid', 'orders']
